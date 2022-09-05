684e7c556dc665ed16593ab3698b7c892b5d1a47
hamed aljamry <hamed970@hotmail.com>
2022-09-06 00:39:46 +0100

    show/new/controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -5,7 +5,7 @@ class WorkoutsController < ApplicationController
 
 
   def show
-    @workout = Workout.find(params(:id))
+    @workout = Workout.find(params[:id])
   end
 
   def new
@@ -13,5 +13,19 @@ class WorkoutsController < ApplicationController
   end
 
   def create
+    @workout = Workout.new(workout_params)
+    @workout.user = current_user
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def workout_params
+	  params.require(:workout).permit(:exercise, :set, :rep, :weight, :date)
+  end
+
 end
