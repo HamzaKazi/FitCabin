7c955d8c65a39fc55fac60639bbc7ce0fbd29ab0
Hamza Kazi <hamzakaziii1@gmail.com>
2022-09-05 17:06:24 +0100

    added functions methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @post = Post.new(post_params)
+    @post.chatroom = @chatroom
+    @post.user = current_user
+    if @post.save
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "post", locals: {post: @post})
+      )
+      head :ok
+    else
+      render "chatrooms/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
