5fee69052280f5464fad4d8c7ed99fa520b6d73e
hamed aljamry <hamed970@hotmail.com>
2022-09-06 15:42:45 +0100

    new model

--- db/migrate/20220905145618_create_workouts.rb
+++ db/migrate/20220905145618_create_workouts.rb
@@ -1,10 +1,6 @@
 class CreateWorkouts < ActiveRecord::Migration[7.0]
   def change
     create_table :workouts do |t|
-      t.string :exercise
-      t.integer :set
-      t.integer :rep
-      t.float :weight
       t.datetime :date
       t.references :user, null: false, foreign_key: true
 
