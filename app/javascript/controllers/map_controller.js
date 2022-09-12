749e3c04340a9dcb2ed0b32e20c3948b1e707d7c
cyrusrochford <cyrus.rochford94@gmail.com>
2022-09-12 11:29:44 +0100

    mapsearch

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -32,9 +32,9 @@ export default class extends Controller {
     });
   }
 
-  // #fitMapToMarkers() {
-  //   const bounds = new mapboxgl.LngLatBounds()
-  //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  // }
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
 }
