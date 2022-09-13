39ca648c2b823e618e1be2f6a00d82096bb329cf
hamed aljamry <hamed970@hotmail.com>
2022-09-13 12:39:46 +0100

    routes for reviews, added user migration

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :gym
+  belongs_to :user
   validates :rating, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_232409) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_113617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,7 +101,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_232409) do
     t.bigint "gym_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["gym_id"], name: "index_reviews_on_gym_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -149,5 +151,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_232409) do
   add_foreign_key "exercises", "workouts"
   add_foreign_key "posts", "users"
   add_foreign_key "reviews", "gyms"
+  add_foreign_key "reviews", "users"
   add_foreign_key "workouts", "users"
 end
